942768202180804e71ded2c7fb5fc33921ec6db6
Michelle Zulli <mzchellz@gmail.com>
2015-05-07 04:46:52 -0400

    First commit

---
+++ Songwriter Project/frmMain.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblKey = New System.Windows.Forms.Label()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.grpBase = New System.Windows.Forms.GroupBox()
+        Me.radB = New System.Windows.Forms.RadioButton()
+        Me.radC = New System.Windows.Forms.RadioButton()
+        Me.radD = New System.Windows.Forms.RadioButton()
+        Me.radE = New System.Windows.Forms.RadioButton()
+        Me.radF = New System.Windows.Forms.RadioButton()
+        Me.radG = New System.Windows.Forms.RadioButton()
+        Me.radA = New System.Windows.Forms.RadioButton()
+        Me.txtMajorScale = New System.Windows.Forms.TextBox()
+        Me.lblProgressions = New System.Windows.Forms.Label()
+        Me.lblScales = New System.Windows.Forms.Label()
+        Me.txtMajorProgs = New System.Windows.Forms.TextBox()
+        Me.grpAccidental = New System.Windows.Forms.GroupBox()
+        Me.radNatural = New System.Windows.Forms.RadioButton()
+        Me.radFlat = New System.Windows.Forms.RadioButton()
+        Me.radSharp = New System.Windows.Forms.RadioButton()
+        Me.grpBase.SuspendLayout()
+        Me.grpAccidental.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(343, 400)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 35)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblKey
+        '
+        Me.lblKey.BackColor = System.Drawing.SystemColors.Window
+        Me.lblKey.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblKey.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblKey.Location = New System.Drawing.Point(350, 29)
+        Me.lblKey.Name = "lblKey"
+        Me.lblKey.Size = New System.Drawing.Size(79, 89)
+        Me.lblKey.TabIndex = 0
+        Me.lblKey.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Location = New System.Drawing.Point(333, 128)
+        Me.btnSelect.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(112, 35)
+        Me.btnSelect.TabIndex = 1
+        Me.btnSelect.Text = "&Set Key"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'grpBase
+        '
+        Me.grpBase.Controls.Add(Me.radB)
+        Me.grpBase.Controls.Add(Me.radC)
+        Me.grpBase.Controls.Add(Me.radD)
+        Me.grpBase.Controls.Add(Me.radE)
+        Me.grpBase.Controls.Add(Me.radF)
+        Me.grpBase.Controls.Add(Me.radG)
+        Me.grpBase.Controls.Add(Me.radA)
+        Me.grpBase.Location = New System.Drawing.Point(12, 12)
+        Me.grpBase.Name = "grpBase"
+        Me.grpBase.Size = New System.Drawing.Size(158, 151)
+        Me.grpBase.TabIndex = 2
+        Me.grpBase.TabStop = False
+        Me.grpBase.Text = "Select Base Note"
+        '
+        'radB
+        '
+        Me.radB.AutoSize = True
+        Me.radB.Location = New System.Drawing.Point(10, 58)
+        Me.radB.Name = "radB"
+        Me.radB.Size = New System.Drawing.Size(38, 24)
+        Me.radB.TabIndex = 1
+        Me.radB.TabStop = True
+        Me.radB.Text = "&B"
+        Me.radB.UseVisualStyleBackColor = True
+        '
+        'radC
+        '
+        Me.radC.AutoSize = True
+        Me.radC.Location = New System.Drawing.Point(10, 82)
+        Me.radC.Name = "radC"
+        Me.radC.Size = New System.Drawing.Size(38, 24)
+        Me.radC.TabIndex = 2
+        Me.radC.TabStop = True
+        Me.radC.Text = "&C"
+        Me.radC.UseVisualStyleBackColor = True
+        '
+        'radD
+        '
+        Me.radD.AutoSize = True
+        Me.radD.Location = New System.Drawing.Point(10, 106)
+        Me.radD.Name = "radD"
+        Me.radD.Size = New System.Drawing.Size(39, 24)
+        Me.radD.TabIndex = 3
+        Me.radD.TabStop = True
+        Me.radD.Text = "&D"
+        Me.radD.UseVisualStyleBackColor = True
+        '
+        'radE
+        '
+        Me.radE.AutoSize = True
+        Me.radE.Location = New System.Drawing.Point(54, 34)
+        Me.radE.Name = "radE"
+        Me.radE.Size = New System.Drawing.Size(38, 24)
+        Me.radE.TabIndex = 4
+        Me.radE.TabStop = True
+        Me.radE.Text = "&E"
+        Me.radE.UseVisualStyleBackColor = True
+        '
+        'radF
+        '
+        Me.radF.AutoSize = True
+        Me.radF.Location = New System.Drawing.Point(54, 58)
+        Me.radF.Name = "radF"
+        Me.radF.Size = New System.Drawing.Size(37, 24)
+        Me.radF.TabIndex = 5
+        Me.radF.TabStop = True
+        Me.radF.Text = "&F"
+        Me.radF.UseVisualStyleBackColor = True
+        '
+        'radG
+        '
+        Me.radG.AutoSize = True
+        Me.radG.Location = New System.Drawing.Point(54, 82)
+        Me.radG.Name = "radG"
+        Me.radG.Size = New System.Drawing.Size(40, 24)
+        Me.radG.TabIndex = 6
+        Me.radG.TabStop = True
+        Me.radG.Text = "&G"
+        Me.radG.UseVisualStyleBackColor = True
+        '
+        'radA
+        '
+        Me.radA.AutoSize = True
+        Me.radA.Location = New System.Drawing.Point(10, 34)
+        Me.radA.Name = "radA"
+        Me.radA.Size = New System.Drawing.Size(38, 24)
+        Me.radA.TabIndex = 0
+        Me.radA.TabStop = True
+        Me.radA.Text = "&A"
+        Me.radA.UseVisualStyleBackColor = True
+        '
+        'txtMajorScale
+        '
+        Me.txtMajorScale.Location = New System.Drawing.Point(22, 208)
+        Me.txtMajorScale.Name = "txtMajorScale"
+        Me.txtMajorScale.Size = New System.Drawing.Size(433, 26)
+        Me.txtMajorScale.TabIndex = 6
+        '
+        'lblProgressions
+        '
+        Me.lblProgressions.AutoSize = True
+        Me.lblProgressions.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProgressions.Location = New System.Drawing.Point(18, 267)
+        Me.lblProgressions.Name = "lblProgressions"
+        Me.lblProgressions.Size = New System.Drawing.Size(241, 20)
+        Me.lblProgressions.TabIndex = 9
+        Me.lblProgressions.Text = "Common Chord Progressions"
+        '
+        'lblScales
+        '
+        Me.lblScales.AutoSize = True
+        Me.lblScales.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScales.Location = New System.Drawing.Point(18, 185)
+        Me.lblScales.Name = "lblScales"
+        Me.lblScales.Size = New System.Drawing.Size(103, 20)
+        Me.lblScales.TabIndex = 4
+        Me.lblScales.Text = "Major Scale"
+        '
+        'txtMajorProgs
+        '
+        Me.txtMajorProgs.Location = New System.Drawing.Point(22, 290)
+        Me.txtMajorProgs.Multiline = True
+        Me.txtMajorProgs.Name = "txtMajorProgs"
+        Me.txtMajorProgs.Size = New System.Drawing.Size(293, 145)
+        Me.txtMajorProgs.TabIndex = 11
+        '
+        'grpAccidental
+        '
+        Me.grpAccidental.Controls.Add(Me.radSharp)
+        Me.grpAccidental.Controls.Add(Me.radFlat)
+        Me.grpAccidental.Controls.Add(Me.radNatural)
+        Me.grpAccidental.Location = New System.Drawing.Point(188, 12)
+        Me.grpAccidental.Name = "grpAccidental"
+        Me.grpAccidental.Size = New System.Drawing.Size(127, 150)
+        Me.grpAccidental.TabIndex = 3
+        Me.grpAccidental.TabStop = False
+        Me.grpAccidental.Text = "Accidental?"
+        '
+        'radNatural
+        '
+        Me.radNatural.AutoSize = True
+        Me.radNatural.Location = New System.Drawing.Point(6, 34)
+        Me.radNatural.Name = "radNatural"
+        Me.radNatural.Size = New System.Drawing.Size(78, 24)
+        Me.radNatural.TabIndex = 0
+        Me.radNatural.TabStop = True
+        Me.radNatural.Text = "&Natural"
+        Me.radNatural.UseVisualStyleBackColor = True
+        '
+        'radFlat
+        '
+        Me.radFlat.AutoSize = True
+        Me.radFlat.Location = New System.Drawing.Point(6, 58)
+        Me.radFlat.Name = "radFlat"
+        Me.radFlat.Size = New System.Drawing.Size(77, 24)
+        Me.radFlat.TabIndex = 1
+        Me.radFlat.TabStop = True
+        Me.radFlat.Text = "F&lat (b)"
+        Me.radFlat.UseVisualStyleBackColor = True
+        '
+        'radSharp
+        '
+        Me.radSharp.AutoSize = True
+        Me.radSharp.Location = New System.Drawing.Point(6, 82)
+        Me.radSharp.Name = "radSharp"
+        Me.radSharp.Size = New System.Drawing.Size(93, 24)
+        Me.radSharp.TabIndex = 2
+        Me.radSharp.TabStop = True
+        Me.radSharp.Text = "Sharp (&#)"
+        Me.radSharp.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 474)
+        Me.Controls.Add(Me.grpAccidental)
+        Me.Controls.Add(Me.txtMajorProgs)
+        Me.Controls.Add(Me.lblScales)
+        Me.Controls.Add(Me.lblKey)
+        Me.Controls.Add(Me.lblProgressions)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.txtMajorScale)
+        Me.Controls.Add(Me.grpBase)
+        Me.Controls.Add(Me.btnExit)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.Text = "Songwriter's Assistant"
+        Me.grpBase.ResumeLayout(False)
+        Me.grpBase.PerformLayout()
+        Me.grpAccidental.ResumeLayout(False)
+        Me.grpAccidental.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblKey As System.Windows.Forms.Label
+    Friend WithEvents btnSelect As System.Windows.Forms.Button
+    Friend WithEvents grpBase As System.Windows.Forms.GroupBox
+    Friend WithEvents radA As System.Windows.Forms.RadioButton
+    Friend WithEvents radB As System.Windows.Forms.RadioButton
+    Friend WithEvents radC As System.Windows.Forms.RadioButton
+    Friend WithEvents radD As System.Windows.Forms.RadioButton
+    Friend WithEvents radE As System.Windows.Forms.RadioButton
+    Friend WithEvents radF As System.Windows.Forms.RadioButton
+    Friend WithEvents radG As System.Windows.Forms.RadioButton
+    Friend WithEvents txtMajorScale As System.Windows.Forms.TextBox
+    Friend WithEvents lblProgressions As System.Windows.Forms.Label
+    Friend WithEvents lblScales As System.Windows.Forms.Label
+    Friend WithEvents txtMajorProgs As System.Windows.Forms.TextBox
+    Friend WithEvents grpAccidental As System.Windows.Forms.GroupBox
+    Friend WithEvents radSharp As System.Windows.Forms.RadioButton
+    Friend WithEvents radFlat As System.Windows.Forms.RadioButton
+    Friend WithEvents radNatural As System.Windows.Forms.RadioButton
+
+End Class
---
+++ Songwriter Project/frmMain.vb
@@ -0,0 +1,269 @@
+﻿' Name:         Songwriting Assistant Project
+' Purpose:      Generate scales and chord progressions
+' Programmer:   Michelle Zulli on 2015.04.28
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+
+        Dim strBase As String = String.Empty
+        Dim strAccidental As String = String.Empty
+        Dim strKey As String = String.Empty
+        Dim strNotes(6) As String
+        Dim strChords(1, 5) As String
+        Dim intScaleIndex As Integer
+        Dim intChordRowIndex As Integer
+        Dim intChordColIndex As Integer
+
+        ' clear the form
+        lblKey.Text = String.Empty
+        txtMajorScale.Text = String.Empty
+        txtMajorProgs.Text = String.Empty
+
+        ' assign the selected base note to a variable
+        Select Case True
+            Case radA.Checked
+                strBase = "A"
+            Case radB.Checked
+                strBase = "B"
+            Case radC.Checked
+                strBase = "C"
+            Case radD.Checked
+                strBase = "D"
+            Case radE.Checked
+                strBase = "E"
+            Case radF.Checked
+                strBase = "F"
+            Case radG.Checked
+                strBase = "G"
+            Case Else
+                MessageBox.Show("Select a base note.",
+                                "Songwriter's Assistant",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information)
+        End Select
+
+        ' decide if the note is flat or sharp
+        Select Case True
+            Case radFlat.Checked
+                strAccidental = "b"
+            Case radSharp.Checked
+                strAccidental = "#"
+            Case Else
+                strAccidental = String.Empty
+        End Select
+
+        ' display the selected key and clear the selections
+        strKey = strBase & strAccidental
+        lblKey.Text = strKey
+        radA.Checked = False
+        radB.Checked = False
+        radC.Checked = False
+        radD.Checked = False
+        radE.Checked = False
+        radF.Checked = False
+        radG.Checked = False
+        radNatural.Checked = False
+        radFlat.Checked = False
+        radSharp.Checked = False
+
+        ' populate the array of notes
+        If strKey = "A" Then
+            strNotes(0) = "A"
+            strNotes(1) = "B"
+            strNotes(2) = "C#"
+            strNotes(3) = "D"
+            strNotes(4) = "E"
+            strNotes(5) = "F#"
+            strNotes(6) = "G#"
+
+        ElseIf strKey = "Ab" Then
+            strNotes(0) = "Ab"
+            strNotes(1) = "Bb"
+            strNotes(2) = "C"
+            strNotes(3) = "Db"
+            strNotes(4) = "Eb"
+            strNotes(5) = "F"
+            strNotes(6) = "G"
+
+        ElseIf strKey = "B" Then
+            strNotes(0) = "B"
+            strNotes(1) = "C#"
+            strNotes(2) = "D#"
+            strNotes(3) = "E"
+            strNotes(4) = "F#"
+            strNotes(5) = "G#"
+            strNotes(6) = "A#"
+
+        ElseIf strKey = "Bb" Then
+            strNotes(0) = "Bb"
+            strNotes(1) = "C"
+            strNotes(2) = "D"
+            strNotes(3) = "Eb"
+            strNotes(4) = "F"
+            strNotes(5) = "G"
+            strNotes(6) = "A"
+
+        ElseIf strKey = "C" Then
+            strNotes(0) = "C"
+            strNotes(1) = "D"
+            strNotes(2) = "E"
+            strNotes(3) = "F"
+            strNotes(4) = "G"
+            strNotes(5) = "A"
+            strNotes(6) = "B"
+
+        ElseIf strKey = "Cb" Then
+            strNotes(0) = "Cb"
+            strNotes(1) = "Db"
+            strNotes(2) = "Eb"
+            strNotes(3) = "Fb"
+            strNotes(4) = "Gb"
+            strNotes(5) = "Ab"
+            strNotes(6) = "Bb"
+
+        ElseIf strKey = "C#" Then
+            strNotes(0) = "C#"
+            strNotes(1) = "D#"
+            strNotes(2) = "E#"
+            strNotes(3) = "F#"
+            strNotes(4) = "G#"
+            strNotes(5) = "A#"
+            strNotes(6) = "B#"
+
+        ElseIf strKey = "D" Then
+            strNotes(0) = "D"
+            strNotes(1) = "E"
+            strNotes(2) = "F#"
+            strNotes(3) = "G"
+            strNotes(4) = "A"
+            strNotes(5) = "B"
+            strNotes(6) = "C#"
+
+        ElseIf strKey = "Db" Then
+            strNotes(0) = "Db"
+            strNotes(1) = "Eb"
+            strNotes(2) = "F"
+            strNotes(3) = "Gb"
+            strNotes(4) = "Ab"
+            strNotes(5) = "Bb"
+            strNotes(6) = "C"
+
+        ElseIf strKey = "E" Then
+            strNotes(0) = "E"
+            strNotes(1) = "F#"
+            strNotes(2) = "G#"
+            strNotes(3) = "A"
+            strNotes(4) = "B"
+            strNotes(5) = "C#"
+            strNotes(6) = "D#"
+
+        ElseIf strKey = "Eb" Then
+            strNotes(0) = "Eb"
+            strNotes(1) = "F"
+            strNotes(2) = "G"
+            strNotes(3) = "Ab"
+            strNotes(4) = "Bb"
+            strNotes(5) = "C"
+            strNotes(6) = "D"
+
+        ElseIf strKey = "F" Then
+            strNotes(0) = "F"
+            strNotes(1) = "G"
+            strNotes(2) = "A"
+            strNotes(3) = "Bb"
+            strNotes(4) = "C"
+            strNotes(5) = "D"
+            strNotes(6) = "E"
+
+        ElseIf strKey = "Fb" Then
+            strNotes(0) = "Fb"
+            strNotes(1) = "Gb"
+            strNotes(2) = "Ab"
+            strNotes(3) = "Bbb"
+            strNotes(4) = "Cb"
+            strNotes(5) = "Db"
+            strNotes(6) = "Eb"
+
+        ElseIf strKey = "F#" Then
+            strNotes(0) = "F#"
+            strNotes(1) = "G#"
+            strNotes(2) = "A#"
+            strNotes(3) = "B"
+            strNotes(4) = "C#"
+            strNotes(5) = "D#"
+            strNotes(6) = "E#"
+
+        ElseIf strKey = "G" Then
+            strNotes(0) = "G"
+            strNotes(1) = "A"
+            strNotes(2) = "B"
+            strNotes(3) = "C"
+            strNotes(4) = "D"
+            strNotes(5) = "E"
+            strNotes(6) = "F#"
+
+        ElseIf strKey = "Gb" Then
+            strNotes(0) = "Gb"
+            strNotes(1) = "Ab"
+            strNotes(2) = "Bb"
+            strNotes(3) = "Cb"
+            strNotes(4) = "Db"
+            strNotes(5) = "Eb"
+            strNotes(6) = "F"
+
+        ElseIf strKey = "G#" Then
+            strNotes(0) = "G#"
+            strNotes(1) = "A#"
+            strNotes(2) = "B#"
+            strNotes(3) = "C#"
+            strNotes(4) = "D#"
+            strNotes(5) = "E#"
+            strNotes(6) = "F##"
+
+        Else
+            MessageBox.Show("Invalid key. Try again.",
+                                "Songwriter's Assistant",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information)
+        End If
+
+        ' populate the array of chords
+        strChords(0, 0) = "1950s"
+        strChords(0, 1) = strNotes(0)
+        strChords(0, 2) = strNotes(5) & "-minor"
+        strChords(0, 3) = strNotes(4)
+        strChords(0, 4) = strNotes(3)
+
+        strChords(1, 0) = "Blues, Pop, Punk"
+        strChords(1, 1) = strNotes(0)
+        strChords(1, 2) = strNotes(3)
+        strChords(1, 3) = strNotes(4)
+
+        ' display the scale
+        intScaleIndex = 0
+        Do Until intScaleIndex > 6
+            txtMajorScale.Text = txtMajorScale.Text.Trim & "   " & strNotes(intScaleIndex)
+            intScaleIndex += 1
+        Loop
+
+        ' display the chord progressions
+        intChordRowIndex = 0
+        Do Until intChordRowIndex > 1
+            txtMajorProgs.Text = txtMajorProgs.Text & strChords(intChordRowIndex, 0) & ":" &
+                ControlChars.NewLine
+            intChordColIndex = 1
+            Do Until intChordColIndex > 5
+                txtMajorProgs.Text = txtMajorProgs.Text & " " &
+                    strChords(intChordRowIndex, intChordColIndex)
+                intChordColIndex += 1
+            Loop
+            txtMajorProgs.Text = txtMajorProgs.Text & ControlChars.NewLine & ControlChars.NewLine
+            intChordRowIndex += 1
+        Loop
+    End Sub
+End Class
